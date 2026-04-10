a1e1234d75f7a261524afd655938235e48f76a75
Hassan Hussien <hakpb7@gmail.com>
2026-04-09 17:11:56 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,6 @@ export default function App() {
   return (
     <div>
       <h1>Compound Component Pattern</h1>
-      {/* <Counter
-        iconIncrease="+"
-        iconDecrease="-"
-        label="My NOT so flexible counter"
-        hideLabel={false}
-        hideIncrease={false}
-        hideDecrease={false}
-      /> */}
       <Counter>
         <Counter.label>create by Compound Component Pattern</Counter.label>
         <Counter.increase icon="+" />
